0a125ba9406b02bde3d37142b137e361569e57e3
Anton Havrenkov <selmernoid@gmail.com>
2021-10-03 20:21:08 +0300

    4.1 titlecase

--- src/app/features/products/services/products.service.ts
+++ src/app/features/products/services/products.service.ts
@@ -14,6 +14,7 @@ export class ProductsService {
       { id: 1960, name: 'Zaporozhec', category: CategoryType.Car, isAvailable: false, price: 15.65 },
       { id: 8, name: 'Icecream', category: CategoryType.Food, isAvailable: true, price: 12.2 },
       { id: 100, name: 'AlphaZero', category: CategoryType.Software, isAvailable: false, price: 150000 },
+      { id: 120, name: 'lower-case item', category: CategoryType.Software, isAvailable: false, price: 150000 },
     ];
   }
 }
