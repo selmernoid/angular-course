f35f12aaa07df6e75085d9fb6119314b9e888c81
Anton Havrenkov <selmernoid@gmail.com>
2021-10-11 23:53:02 +0300

    5.1 Product list

--- src/app/shp-routing.module.ts
+++ src/app/shp-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './features/products/components/product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'products-list',
+    data: { title: 'Products list' },
+    component: ProductListComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'products-list',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
